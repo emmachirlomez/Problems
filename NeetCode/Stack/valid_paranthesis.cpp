2a0b87c8983f454d7da5885741aaf726fb16e81f
Emma Chirlomez <emma.chirlomez@gmail.com>
2023-12-23 01:17:39 +0200

    Day 13 - Started Stack Module

--- NeetCode/Stack/valid_paranthesis.cpp
+++ NeetCode/Stack/valid_paranthesis.cpp
@@ -21,6 +21,23 @@ bool isValid(string s) {
     return stack.empty();
 }
 
+bool isValidSecond(string s) {
+    stack<char> stack;
+    unordered_map<char, char> paran = {{')', '('}, {']', '['}, {'}', '{'}};
+    for (auto i : s) {
+        if (paran.find(i) == paran.end())
+            stack.push(i);
+        else {
+            if (stack.empty())
+                return false;
+            else if (stack.top() == paran[i])
+                stack.pop();
+            else return false;
+        }
+    }
+    return stack.empty();
+}
+
 int main() {
     string s = "(]";
     cout << isValid(s) << endl;
